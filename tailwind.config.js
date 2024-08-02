4c7a8e8442ae4740097bf8389cc52504503e19ab
nexusboyko <alexboyko@outlook.com>
2024-08-02 14:35:10 -0700

    Setup tailwind CSS styling

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'internal/templates/*.templ',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mono': ['JetBrains Mono', 'Courier New', 'monospace'],
+      },
+      colors: {
+        primary: colors.blue,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
